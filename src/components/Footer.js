8ff685b4f1885f4f025b072fa196b678e86ec790
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-06-23 04:21:41 +0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-title">PHIM ONLINE</p>
+        <p className="footer-subtitle">Kho phim chất lượng cao miễn phí</p>
+        <p className="footer-designer">Thiết kế bởi Mạnh Dương</p>
+        <p className="footer-copyright">Copyright © 2025 All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
