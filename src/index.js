1d79cd181d811bdf32638fc47c568384d8dddc35
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-06-23 06:54:05 +0700

    Loại bỏ thông báo

--- src/index.js
+++ src/index.js
@@ -15,10 +15,8 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
       })
       .catch(registrationError => {
-        console.log('Service Worker registration failed:', registrationError);
       });
   });
 }
