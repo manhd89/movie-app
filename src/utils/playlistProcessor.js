39708a115072195fd2b4d589d372dbb89e14fdaf
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-07-12 21:50:51 +0700

    Rút gọn code

--- src/utils/playlistProcessor.js
+++ src/utils/playlistProcessor.js
@@ -8,8 +8,6 @@ export const config = {
     /#EXT-X-DISCONTINUITY\n(?:#EXT-X-KEY:METHOD=NONE\n(?:.*\n){18,24})?#EXT-X-DISCONTINUITY\n|convertv7\//g,
     /#EXT-X-DISCONTINUITY\n(?:#EXTINF:(?:3.92|0.76|2.00|2.50|2.00|2.42|2.00|0.78|1.96)0000,\n.*\n){9}#EXT-X-DISCONTINUITY\n(?:#EXTINF:(?:2.00|1.76|3.20|2.00|1.36|2.00|2.00|0.72)0000,\n.*\n){8}(?=#EXT-X-DISCONTINUITY)/g,
   ],
-  // Danh sách các tên miền bỏ qua việc lọc quảng cáo (tùy chọn)
-  domainBypassWhitelist: ['kkphimplayer', 'phim1280', 'opstream'],
 };
 
 // Kiểm tra xem playlist có chứa quảng cáo không
@@ -24,18 +22,6 @@ export function isContainAds(playlist) {
 export function getTotalDuration(playlist) {
   const matches = playlist.match(/#EXTINF:([\d.]+)/g) ?? [];
   return matches.reduce((sum, match) => sum + parseFloat(match.split(':')[1]), 0);
-}
-
-// Lấy thời lượng ngoại lệ cho một số URL cụ thể (chỉ dùng trong Service Worker hiện tại)
-export function getExceptionDuration(url) {
-  const parsedUrl = new URL(url);
-  if (['ophim', 'opstream'].some((keyword) => parsedUrl.hostname.includes(keyword))) {
-    return 600; // Ví dụ: 10 phút
-  } else if (['nguonc', 'streamc'].some((keyword) => parsedUrl.hostname.includes(keyword))) {
-    return Infinity; // Vô hạn, không chặn quảng cáo từ nguồn này
-  } else {
-    return 900; // Mặc định: 15 phút
-  }
 }
 
 // Hàm fetch và loại bỏ quảng cáo (dùng cả trong Service Worker và MovieDetail)
@@ -66,15 +52,6 @@ export async function processPlaylistForAds(playlistUrl) {
         return line;
       }
     });
-
-    // Handle master playlist (fetch sub-playlist)
-    if (playlist.includes("#EXT-X-STREAM-INF")) {
-      const variantUrlMatch = playlist.split('\n').find(line => !line.startsWith('#') && line.trim() !== '');
-      if (variantUrlMatch) {
-          const normalizedVariantUrl = new URL(variantUrlMatch, baseUrl).href.replace(/^http:/, "https:");
-          return await processPlaylistForAds(normalizedVariantUrl); // Recursive call for sub-playlist
-      }
-    }
 
     // Remove ads if detected
     if (isContainAds(playlist)) {
