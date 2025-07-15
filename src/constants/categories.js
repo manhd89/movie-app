a0eceb66151d4d548fef392112a6ed4822405236
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-07-15 13:32:28 +0700

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,11 @@
+// src/constants/categories.js
+export const CATEGORIES_MAPPING = [
+    { slug: 'phim-moi-cap-nhat', name: 'Phim Mới Cập Nhật' },
+    { slug: 'phim-bo', name: 'Phim Bộ' },
+    { slug: 'phim-le', name: 'Phim Lẻ' },
+    { slug: 'tv-shows', name: 'TV Shows' },
+    { slug: 'hoat-hinh', name: 'Hoạt Hình' },
+    { slug: 'phim-vietsub', name: 'Phim Vietsub' },
+    { slug: 'phim-thuyet-minh', name: 'Phim Thuyết Minh' },
+    { slug: 'phim-long-tieng', name: 'Phim Lồng Tiếng' },
+];
